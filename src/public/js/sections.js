79ae1143d796abcafe05f57401d881f674eb33d4
theConceptSec <67712102+thetermuxchoice@users.noreply.github.com>
2023-06-16 22:15:20 -0500

    Add files via upload

--- src/public/js/sections.js
+++ src/public/js/sections.js
@@ -1,25 +1,39 @@
-gsap.registerPlugin(ScrollTrigger);
+// I know that the code could be better.
+// If you have some tips or improvement, please let me know.
 
-let getRatio = el => window.innerHeight / (window.innerHeight + el.offsetHeight);
+$('.img-parallax').each(function(){
+  var img = $(this);
+  var imgParent = $(this).parent();
+  function parallaxImg () {
+    var speed = img.data('speed');
+    var imgY = imgParent.offset().top;
+    var winY = $(this).scrollTop();
+    var winH = $(this).height();
+    var parentH = imgParent.innerHeight();
 
-gsap.utils.toArray("section").forEach((section, i) => {
-  section.bg = section.querySelector(".sections-bg"); 
 
-  
-  // the first image (i === 0) should be handled differently because it should start at the very top.
-  // use function-based values in order to keep things responsive
-  gsap.fromTo(section.bg, {
-    backgroundPosition: () => i ? `50% ${-window.innerHeight * getRatio(section)}px` : "50% 0px"
-  }, {
-    backgroundPosition: () => `50% ${window.innerHeight * (1 - getRatio(section))}px`,
-    ease: "none",
-    scrollTrigger: {
-      trigger: section,
-      start: () => i ? "top bottom" : "top top", 
-      end: "bottom top",
-      scrub: true,
-      invalidateOnRefresh: true // to make it responsive
+    // The next pixel to show on screen      
+    var winBottom = winY + winH;
+
+    // If block is shown on screen
+    if (winBottom > imgY && winY < imgY + parentH) {
+      // Number of pixels shown after block appear
+      var imgBottom = ((winBottom - imgY) * speed);
+      // Max number of pixels until block disappear
+      var imgTop = winH + parentH;
+      // Porcentage between start showing until disappearing
+      var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+    }
+    img.css({
+      top: imgPercent + '%',
+      transform: 'translate(-50%, -' + imgPercent + '%)'
+    });
+  }
+  $(document).on({
+    scroll: function () {
+      parallaxImg();
+    }, ready: function () {
+      parallaxImg();
     }
   });
-
 });
